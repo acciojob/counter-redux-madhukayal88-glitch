8ed9509d8c6e62589942dba673136ede4c22adfa
madhukayal88-glitch <madhukayal88@gmail.com>
2026-09-09 12:19:00 +0530

    Fix Redux file structure and setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/counterSlice';
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+    </div>
+  );
+}
+
+export default App;
